0357c54b9ad927834b42c8d592c60d95dcd7926f
RKS <ronitkrshah@tuta.io>
2025-02-16 13:41:11 +0530

    feat: dont catch unexpected errors

--- app.lua
+++ app.lua
@@ -17,10 +17,11 @@ router(app)
 
 -- Handle Unmatched Routes
 app:get("/*", function()
-	return error_response(404, "The requested resource was not found", "Invalid Api Endpoint")
+	return error_response(
+		404,
+		"Route Not Found",
+		"The requested endpoint does not exist. Please check the URL and try again."
+	)
 end)
 
-app.handle_error = function(_, err)
-	return error_response(500, err)
-end
 return app
